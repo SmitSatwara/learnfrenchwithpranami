01c9bbc4a38c5e0f2e86721316c79fd821958e8a
Smit <smit.satwara@fdmgroup.com>
2025-08-03 17:26:45 -0300

    Updated url for backend

--- tef-tutor-site/src/components/Contact.jsx
+++ tef-tutor-site/src/components/Contact.jsx
@@ -16,7 +16,7 @@ export default function Contact() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://learnfrenchwithpranami-backend.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
